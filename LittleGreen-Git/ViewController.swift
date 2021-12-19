ead1982ba219cc352e987da959ab7647f4b6dc31
xiayi <xiayiaxy@icloud.com>
2021-12-19 15:25:26 +0800

    做了UI

--- LittleGreen-Git/ViewController.swift
+++ LittleGreen-Git/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("做了UI")
 
         // Do any additional setup after loading the view.
     }
